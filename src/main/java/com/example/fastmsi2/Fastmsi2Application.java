6b138c14a406836d36003f1b2305c4bf367b8ab5
anjaehun <juhun104@naver.com>
2023-07-05 19:44:57 +0900

    fastmls 초기화 설정

---
+++ src/main/java/com/example/fastmsi2/Fastmsi2Application.java
@@ -0,0 +1,13 @@
+package com.example.fastmsi2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fastmsi2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Fastmsi2Application.class, args);
+    }
+
+}
