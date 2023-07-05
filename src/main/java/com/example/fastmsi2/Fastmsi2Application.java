645a8c1da607739e3f023f7b1fe21ca441777c1e
anjaehun <juhun104@naver.com>
2023-07-05 19:49:36 +0900

    시스템

--- src/main/java/com/example/fastmsi2/Fastmsi2Application.java
+++ src/main/java/com/example/fastmsi2/Fastmsi2Application.java
@@ -3,6 +3,7 @@ package com.example.fastmsi2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+ // 2023.07.05
 @SpringBootApplication
 public class Fastmsi2Application {
 
